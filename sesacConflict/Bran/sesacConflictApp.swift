45b5459d5472b01cbab02adc36d80a5958e82dd0
seunghyun Lee <yoruck2@gmail.com>
2024-10-08 13:41:32 +0900

    init

---
+++ sesacConflict/Bran/sesacConflictApp.swift
@@ -0,0 +1,17 @@
+//
+//  sesacConflictApp.swift
+//  sesacConflict
+//
+//  Created by dopamint on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct sesacConflictApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
